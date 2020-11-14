255d06785a3be81f2e5ddf8f82d6a4856abc0046
Gadacsi Akos <akosgadacsi99@gmail.com>
2020-11-14 23:15:41 +0100

    found problem. 2 repo was not public also                             renamed some methods

--- MVC/Logic/SystemLogic.cs
+++ MVC/Logic/SystemLogic.cs
@@ -22,29 +22,42 @@ namespace Logic
 
         //crud methods
 
-        public void AddVideo(Models.System item)
+        public void AddSystem(Models.System item)
         {
             this.systemRepo.Add(item);
         }
 
-        public void DeleteVideo(string ID)
+        public void DeleteSystem(string ID)
         {
             this.systemRepo.Delete(ID);
         }
 
-        public IQueryable<Models.System> GetAllVideo()
+        public IQueryable<Models.System> GetAllSystem()
         {
             return systemRepo.Read();
         }
 
-        public Models.System GetVideo(string ID)
+        public Models.System GetSystem(string ID)
         {
             return systemRepo.Read(ID);
         }
 
-        public void UpdateVideo(string uid, Models.System newitem)
+        public void UpdateSystem(string ID, Models.System newitem)
         {
-            systemRepo.Update(uid, newitem);
+            systemRepo.Update(ID, newitem);
         }
+
+        //public void FillDbWithSamples()
+        //{
+        //    Planet p1 = new Planet() { Name = "Earth", Habitable = true, Population = 80000, PlanetType = PlanetType.Terran, PlanetID = Guid.NewGuid().ToString() };
+        //    Planet p2 = new Planet() { Name = "Mars", Habitable = true, Population = 1, PlanetType = PlanetType.Subterran, PlanetID = Guid.NewGuid().ToString() };
+        //    Planet p3 = new Planet() { Name = "Venus", Habitable = false, Population = 0, PlanetType = PlanetType.Miniterran, PlanetID = Guid.NewGuid().ToString() };
+        //    Planet p4 = new Planet() { Name = "Merkur", Habitable = false, Population = 0, PlanetType = PlanetType.Jovian, PlanetID = Guid.NewGuid().ToString() };
+        //    Planet p5 = new Planet() { Name = "Neptunus", Habitable = false, Population = 0, PlanetType = PlanetType.Neptunian, PlanetID = Guid.NewGuid().ToString() };
+
+        //    Star s1 = new Star() { Name = "Our Sun", Age = 10, StarType = StarType.Orange_Dwarfs, StarID = Guid.NewGuid().ToString() };
+
+
+        //}
     }
 }
