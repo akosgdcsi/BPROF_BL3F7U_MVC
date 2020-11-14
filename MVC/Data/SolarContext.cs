4d219c3937f8a9f028c5c42c4a3560c53b30feaf
Gadacsi Akos <akosgadacsi99@gmail.com>
2020-11-14 01:27:10 +0100

    created data class libary and added mdf files

---
+++ MVC/Data/SolarContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace Data
+{
+    public class SolarContext : DbContext
+    {
+        public SolarContext(DbContextOptions<SolarContext> opt):base(opt)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.
+                    UseLazyLoadingProxies().
+                    UseSqlServer(@"data source=(LocalDB)\MSSQLLocalDB;attachdbfilename=|DataDirectory|\Solar.mdf;integrated security=True;MultipleActiveResultSets=True");
+            }
+        }
+        public DbSet<Planet> Planets { get; set; }
+        public DbSet<Star> Stars { get; set; }
+        public DbSet<Models.System> Systems { get; set; }
+    }
+}
